9a6cc7b1c01e194417732c89696ddf6b89faf7e7
Tom Golden <8162045+tbjgolden@users.noreply.github.com>
2021-06-24 19:33:38 -0500

    nush

---
+++ .scripts/brew.sh
@@ -0,0 +1,12 @@
+BREW="jq direnv woff2 node yarn starship nushell"
+CASK="rectangle vscodium kitty firefox"
+
+for brew in $( echo $BREW | xargs ); do
+  echo -e "\033[0;36m$brew\033[0m"
+  brew install $brew
+done
+
+for cask in $( echo $CASK | xargs ); do
+  echo -e "\033[0;36m$cask\033[0m"
+  brew install --cask $cask
+done
