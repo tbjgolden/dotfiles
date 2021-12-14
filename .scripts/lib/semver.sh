b4e3f6284e5311d9c0b56d2e91f386447d5ccaf8
Tom <8162045+tbjgolden@users.noreply.github.com>
2021-12-14 22:23:44 +0000

    streetcred

---
+++ .scripts/lib/semver.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env sh
+
+semver_regex() {
+  local VERSION="([0-9]+)[.]([0-9]+)[.]([0-9]+)"
+  local INFO="([0-9A-Za-z-]+([.][0-9A-Za-z-]+)*)"
+  local PRERELEASE="(-${INFO})"
+  local METAINFO="([+]${INFO})"
+  echo "^${VERSION}${PRERELEASE}?${METAINFO}?$"
+}
+
+SEMVER_REGEX=`semver_regex`
+unset -f semver_regex
+
+semver_check() {
+  echo $1 | grep -Eq "$SEMVER_REGEX"
+}
+
+semver_parse() {
+  semver_check "$1" &&
+  echo $1 | sed -E -e "s/$SEMVER_REGEX/\1 \2 \3 \5 \8/" -e 's/  / _ /g' -e 's/ $/ _/'
+}
+
+semver_compare() {
+  if ! (semver_check "$1" && semver_check "$2"); then
+    return 3
+  fi
+
+  local A=`semver_parse "$1"`
+  local B=`semver_parse "$2"`
+  set $A $B
+
+  if [ $1 -gt $6 ]; then
+    return 1;
+  elif [ $1 -lt $6 ]; then
+    return 2;
+  elif [ $2 -gt $7 ]; then
+    return 1;
+  elif [ $2 -lt $7 ]; then
+    return 2;
+  elif [ $3 -gt $8 ]; then
+    return 1;
+  elif [ $3 -lt $8 ]; then
+    return 2;
+  else
+    return 0;
+  fi
+}
+
+semver_compare $1 $2
--- .scripts/sync.zsh
+++ .scripts/sync.zsh
@@ -1,6 +1,31 @@
 source $HOME/.scripts/env.sh
 
 # if config not set already
+git config --global user.name "Tom"
+git config --global user.email 8162045+tbjgolden@users.noreply.github.com
+git config --global pull.rebase false
+
+if [ "$XDG_CURRENT_DESKTOP" = "KDE" ]; then
+  GIT_VERSION=`git --version | xargs`
+  GIT_VERSION="${GIT_VERSION:12}"
+  echo "$GIT_VERSION"
+
+  sh `dirname ${BASH_SOURCE:-$0}`/lib/semver.sh $GIT_VERSION 2.10.0
+
+  local LAST_EXIT_CODE=$?
+  if [[ $LAST_EXIT_CODE -lt 2 ]]; then
+    git config --global credential.helper libsecret
+  elif []; then
+    git config --global credential.helper store
+  fi
+elif [ `uname` = "Darwin" ]; then
+  git config --global credential.helper osxkeychain
+else
+  git config --global credential.helper store
+fi
+
+exit 1
+
 fur config status.showUntrackedFiles no
 
 # if upstream not already set
@@ -30,5 +55,12 @@ for aur in $( echo $AUR | xargs ); do
 done
 
 # if macos
+
+
+# else throw error
+
+###
+
+# expand dotfileSrc, sync changes, user interactive allowing a diff on each file
 
 
