697cfda39644ab769408e8cf886b2ed2281a4524
Tom <8162045+tbjgolden@users.noreply.github.com>
2022-05-05 20:34:04 +0100

    aur bug

--- .scripts/lib/aur.js
+++ .scripts/lib/aur.js
@@ -2,14 +2,17 @@
 if (process.argv.length < 3) process.exit(1);
 
 const path = require('path')
+const os = require('os')
 const fs = require('fs/promises')
 
 ;(async () => {
   try {
     const package = process.argv[2]
-    const dirPath = path.join(__dirname, '.tmp', package)
+    const dirPath = path.join(os.homedir(), '.aurCache', package)
     const list = await fs.readdir(dirPath)
     const match = list.find(name => name.startsWith(package) && name.includes('.tar'))
-    if (match !== undefined) console.log(path.join(__dirname, '.tmp', package, match))
-  } catch (err) {}
+    if (match !== undefined) console.log(path.join(dirPath, match))
+  } catch (err) {
+    console.error(err)
+  }
 })()
